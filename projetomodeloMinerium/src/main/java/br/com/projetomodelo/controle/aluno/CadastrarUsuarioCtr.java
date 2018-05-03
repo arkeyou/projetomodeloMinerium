65cffbd6345bbd0cb861e29a3504006787091597
Erico Gomes <ericogomez@gmail.com>
2018-05-03 17:41:36 -0300

    Inclusão de radio na tela

--- projetomodeloMinerium/src/main/java/br/com/projetomodelo/controle/aluno/CadastrarUsuarioCtr.java
+++ projetomodeloMinerium/src/main/java/br/com/projetomodelo/controle/aluno/CadastrarUsuarioCtr.java
@@ -9,6 +9,7 @@ import org.zkoss.zk.ui.Component;
 import br.com.projetomodelo.controle.ProjetoModeloBaseCtr;
 import br.com.projetomodelo.entidades.aluno.UsuarioVO;
 import br.com.projetomodelo.enums.TipoContaEnum;
+import br.gov.prodigio.comum.ContextParameters;
 import br.gov.prodigio.controle.componente.Bandboxbind;
 
 public class CadastrarUsuarioCtr extends ProjetoModeloBaseCtr<UsuarioVO> {
@@ -18,11 +19,19 @@ public class CadastrarUsuarioCtr extends ProjetoModeloBaseCtr<UsuarioVO> {
 		super.doAfterCompose(tela);
 		pesquisar();
 	}
-	
+	//Necessário para bandboxbind
 	public List<TipoContaEnum> listarTipoContaEnum(Bandboxbind bandbox) {
 		TipoContaEnum[] values = TipoContaEnum.values();
 		ArrayList<TipoContaEnum> arrayList = new ArrayList<TipoContaEnum>(Arrays.asList(values));
 		return arrayList;
+	}
+
+	@Override
+	protected void configuraTelaDeEntrada(Component comp) throws Exception {
+		super.configuraTelaDeEntrada(comp);
+		//Necessário para campo radiogroupbind
+		getWindowAtual().setAttribute(ContextParameters.PREFIX_ENUM+TipoContaEnum.class.getSimpleName(), new ArrayList<TipoContaEnum>(Arrays.asList(TipoContaEnum.values())), getWindowAtual().APPLICATION_SCOPE);
+
 	}
 	
 }
